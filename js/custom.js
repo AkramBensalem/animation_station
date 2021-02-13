8e68be9d59dc68c5e8c42bbea4a2a7c9aa3f5395
Akram Bensalem <ak.bensalem@esi-sba.dz>
2021-02-13 14:44:56 +0100

    Ajoutez des fichiers projet.

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+
+  $(function () {
+    $('.nav-link').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    AOS.init({
+      disable: 'mobile',
+      duration: 600,
+      anchorPlacement: 'center-bottom'
+    });
+
+    $(function() {
+      $('.nav-link').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+          }, 800);
+            event.preventDefault();
+      });
+    });  
+
+    $('#project-slide').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 20,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          500:{
+              items:2,
+          }
+      }
+    });
+
+  });
+
+
+    
+
